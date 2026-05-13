5ed4fa775566990390dad672e7c35fe15edfdf84
Ivy <ivyzhao335@gmail.com>
2026-05-12 20:23:01 -0400

    update

--- public/scripts/main.js
+++
@@ -1,5 +0,0 @@
-const maxChecked = 1;
-let selectedCategories = 0;
-
-//function that checks if the number of categories checked is greater than the max allowed, if so, uncheck the box and alert user
-document.querySelectorAll("")
