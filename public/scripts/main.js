50a3daebdc5be1739e1974a31ad3e77a4654d348
Ivy <ivyzhao335@gmail.com>
2026-05-14 17:12:39 -0400

    code for map, still not rendering

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,20 @@
 //Link to documentation currently using: https://docs.anychart.com/Basic_Charts/Network_Graph
-let jsonResponse = await fetch ("/data")
+let jsonResponse = await fetch("/data")
 let myNodes = []
 let myEdges = []
 
-for(let individualSubmission of jsonResponse){
-    myNodes.push({id: individualSubmission._id})
+for (let i = 0; i < jsonResponse.newData.length; i++) {
+    myNodes.push({
+        id: jsonResponse.newData[i]._id
+    })
 
-
-    myEdges.push({from: individualSubmission.sadnessCount, to: individualSubmission.joyCount})
+    myEdges.push(
+    {from: jsonResponse.newData[i].newDoc._id.sadnessCount,
+    to: jsonResponse.newData[i].newDoc._id.joyCount
+    }, { from: jsonResponse.newData[i].newDoc._id.joyCount, to: jsonResponse.newData[i].newDoc._id.fearCount }, { from: jsonResponse.newData[i].newDoc._id.fearCount, to: jsonResponse.newData[i].newDoc._id.contentCount }, 
+    {from: jsonResponse.newData[i].newDoc._id.contentCount, to: jsonResponse.newData[i].newDoc._id.sadnessCount })
 }
+
 let data = {
     nodes: [myNodes],
     edges: [myEdges]
