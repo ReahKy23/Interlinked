d131985e7ab0b1814bae05159592c29ac28cd165
Gabrielle Asha <167376964+gabasha00@users.noreply.github.com>
2026-05-19 04:45:31 -0400

    Added feature that allows user to click on computer and bring up a basic popup (no styling yet). Added a new javascript file to toggle the popup tab. Added a function to main.js to ensure that main.js does not try to run on any file that does not contain anyChart function.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -71,3 +71,17 @@ window.onload = async () => {
 
     chart.draw()
 }
+
+
+// Async function to make sure AnyChart doesn't try to run on njk files that don't contain it
+// Without this, other javascript files can not load on individual paged
+window.onload = async () => {
+
+  const container = document.getElementById("chartContainer");
+
+  if (!container) return; //Only runs the code above on pages that contain "chartContainer" 
+
+  var chart = anychart.line();
+  chart.container("chartContainer");
+  chart.draw();
+};
