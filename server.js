e7d7143f6e2f128552b0ea0b8cf21188114f7456
ReahKy23 <Kylah.kerr23@gmail.com>
2026-05-20 10:38:41 -0400

    added cookies integration

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const nunjucks = require("nunjucks");
 const nedb = require("@seald-io/nedb");
 const cookieParser = require("cookie-parser");
 
+const expressSession = require("express-session");
+const nedbSessionStore = require("nedb-promises-session-store");
+
 const app = express();
 const database = new nedb({ filename: "data.db", autoload: true });
 const upload = multer({
   dest: "public/uploads",
 });
-
 
 // nunjucks templating
 app.use(express.static("public"));
@@ -22,114 +24,130 @@ nunjucks.configure("views", {
 
 app.use(cookieParser());
 
+const nedbSessionInit = nedbSessionStore({
+  connect: expressSession,
+  filename: "users.db",
+});
+
+app.use(
+  expressSession({
+    store: nedbSessionInit,
+    cookie: {
+      maxAge: 365 * 24 * 60 * 60 * 1000,
+    },
+    secret: "supersecret123",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
 // routes
 app.get("/", (req, res) => {
-  //if there's a cookie, don't make another one, if there isn't, make one
+  let sid = req.cookies['connect.sid'];
+  let userId = sid ? sid.slice(2) : null;
+  console.log("User ID:", userId);
+
   if (req.cookies.visits) {
     let visits = req.cookies.visits;
-    console.log(req.cookies.visits)
     res.cookie("visits", visits, {
-      //cookie lasts for 10 hours
-      expires: new Date(Date.now() + 1000 * 600 * 60)
-    })
-    //res.redirect("/map")
+      expires: new Date(Date.now() + 1000 * 600 * 60),
+    });
   } else {
-    //3 params:
-    //1. name of cookie stored
-    //2. init val you want to assign
-    //3rd: when the cookie expires, in obj format
-    let oneHrInMs = 1000 * 600 * 60
+    let oneHrInMs = 1000 * 600 * 60;
     res.cookie("visits", 1, {
-      expires: new Date(Date.now() + oneHrInMs)
-    })
+      expires: new Date(Date.now() + oneHrInMs),
+    });
   }
-  res.render("index.njk");
-})
+  res.render("index.njk", { userId: userId });
+});
 
 app.get("/form", (req, res) => {
-  res.render("form.njk")
-})
+  res.render("form.njk");
+});
 
 //route that handles form submission, which allows users to upload and image. after submitting, appends data to database
 app.post("/sign", upload.single("image"), (req, res) => {
-
   //set up initial data to be inserted into database
   let newData = {
     filePath: "uploads/" + req.file.filename,
     name: req.body.userName,
     imgDesc: req.body.caption,
-    categories: [req.body.firstChoice, req.body.secondChoice]
-  }
+    categories: [req.body.firstChoice, req.body.secondChoice],
+  };
 
-  console.log(newData)
+  console.log(newData);
 
   //inserts newData into database
   database.insert(newData, function (err, newDoc) {
     if (err) {
-      console.error(err)
+      console.error(err);
     } else {
       //count how many occurences of each category in the database
-      database.count({ categories: 'sadness' }, (err, sadnessCount) => {
+      database.count({ categories: "sadness" }, (err, sadnessCount) => {
         if (err) {
-          console.error(err)
+          console.error(err);
         }
-        database.count({ categories: 'joy' }, (err, joyCount) => {
+        database.count({ categories: "joy" }, (err, joyCount) => {
           if (err) {
-            console.error(err)
+            console.error(err);
           }
-          database.count({ categories: 'fear' }, (err, fearCount) => {
+          database.count({ categories: "fear" }, (err, fearCount) => {
             if (err) {
-              console.error(err)
+              console.error(err);
             }
-            database.count({ categories: 'content' }, (err, contentCount) => {
+            database.count({ categories: "content" }, (err, contentCount) => {
               if (err) {
-                console.error(err)
+                console.error(err);
               }
-              // newDoc is the newly inserted document, including its _id 
-              database.update({
-                //find id of the newly inserted data
-                _id: newDoc._id
-              },
+              // newDoc is the newly inserted document, including its _id
+              database.update(
+                {
+                  //find id of the newly inserted data
+                  _id: newDoc._id,
+                },
                 //adds the counts of each category to the document
-                { $set: { sadnessCount, joyCount, fearCount, contentCount } }, {}, (err, numReplaced) => {
+                { $set: { sadnessCount, joyCount, fearCount, contentCount } },
+                {},
+                (err, numReplaced) => {
                   if (err) {
-                    console.error(err)
+                    console.error(err);
                   }
-                  res.redirect("/map")
-                })
-            }) //nested counts to ensure that before adding the counts to the database, we have the most updated counts of each category
-          })
-        })
-      })
+                  res.redirect("/map");
+                }
+              );
+            }); //nested counts to ensure that before adding the counts to the database, we have the most updated counts of each category
+          });
+        });
+      });
     }
-  })
-})
+  });
+});
 
 //route that finds one document based on id
 app.get("/data/:id", (req, res) => {
   let query = {
-    _id: req.params.id
-  }
+    _id: req.params.id,
+  };
   database.findOne(query, (err, foundData) => {
-    res.json(foundData)
-  })
-})
+    res.json(foundData);
+  });
+});
 
 //route that renders the map page
 app.get("/map", (req, res) => {
-  res.render("map.njk")
-})
+  res.render("map.njk");
+});
 
 //sends data to the front end via a json format
 app.get("/data", (req, res) => {
-  let query = {}
+  let query = {};
   database.find(query, (err, foundData) => {
-    console.log(foundData)
-    res.json({ newData: foundData })
-  })
-})
+    console.log(foundData);
+    res.json({ newData: foundData });
+  });
+});
 
 // port listening
 app.listen(7001, () => {
-  console.log("server is running on port http://localhost:7001")
-})
+  console.log("server is running on port http://localhost:7001");
+});
